46b2dee76d8f5496b240074bbfe4a8484f74aec8
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-02-23 01:20:14 +0500

    Added functions facade

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ function createBinaryTree(){
     let tree = new Node()
     
     function buildTree (arr){
-        let arrlength = arr.length
-        let midindex = Math.round(arrlength/2)
-        
-        return insert(midindex)
+        while (arr.length >1) {
+            let mid1 = Math.round(arr.length / 2)
+            insert(arr[mid1])
+            let tmp = arr.splice(mid1, arr.length)
+            arr = arr.splice(0, mid1)
+            buildTree(tmp)
+        }
     }
 
     function insert(value, node = tree){
@@ -36,25 +39,63 @@ function createBinaryTree(){
 
     function levelOrder(fn, node = tree){
         if(node.leftNode == null){
-            if(node.rightNode == null){
-                return fn(node.value)
-            }else{
-                levelOrder(fn, node.rightNode)
-            }
+            fn(node)
         }else {
             levelOrder(fn, node.leftNode)
         }
+        if(node.rightNode == null){
+            fn(node)
+        }else{
+            levelOrder(fn, node.rightNode)
+        }
     }
 
-    return {buildTree, insert, levelOrder}
+    function deleteItem(value, node = tree){
+        if(node.leftNode != null && node.leftNode.value == value){
+            tmp = node.leftNode
+        }
+    }
+
+    function find(){
+
+    }
+
+    function inOrder(){
+
+    }
+
+    function preOrder(){
+
+    }
+    
+    function postOrder(){
+
+    }
+
+    function height(){
+
+    }
+
+    function depth(){
+
+    }
+
+    function isBalanced(){
+
+    }
+
+    function reBalance(){
+
+    }
+
+    return {buildTree, insert, levelOrder, find}
 
 }
 
 let data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 let binaryTree = createBinaryTree();
-// binaryTree.buildTree(data)
-binaryTree.insert(3)
-binaryTree.insert(2)
-binaryTree.insert(1)
-binaryTree.levelOrder(console.log)
+binaryTree.buildTree(data)
+// binaryTree.levelOrder(console.log)
+binaryTree.find(23)
+// console.log(binaryTree.tree)
